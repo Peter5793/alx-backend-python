b3f678a7abaf95b1bd90a6d3fee9f7a74cb48d03
Peter5793 <masaba.peter68@gmail.com>
2021-07-23 22:57:40 +0300

    fix pep8

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,6 +2,7 @@
 """ concat function that takes a string 1 and 2 as arguments
 """
 
-def concat(str1: str, str2:str) -> str:
+
+def concat(str1: str, str2: str) -> str:
     """ concat the strings"""
     return str1 + str2
